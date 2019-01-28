4a9fd9c11a0197405c40ae2278b8354acae6cf94
Francososa <franky.sosa@gmail.com>
2019-01-27 23:00:19 -0600

    Define HasResolution type class for fixed 4 digit precision numeric types

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for stockell
+
+## Unreleased changes
---
+++ src/Lib/Types.hs
@@ -0,0 +1,13 @@
+module Types where
+
+
+import Data.Fixed
+
+data E4
+
+instance HasResolution E4 where
+  resolution _ = 10000
+
+type Fixed4 = Fixed E4
+
+
