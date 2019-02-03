99403f42afa1938d6470e4d0b68fc4b3cf811ebc
Francososa <franky.sosa@gmail.com>
2019-02-02 23:30:23 -0600

    Define function to plot line charts for stated QFields in QuotData

--- src/Charts.hs
+++ src/Charts.hs
@@ -10,3 +10,15 @@ import Graphics.Rendering.Chart.Backend.Diagrams (toFile,
 
 import QuoteData
 
+
+plotChart :: (Functor t, Foldable t) =>
+             String -> t QuoteData -> [QField] -> FilePath -> IO ()
+plotChart title quotes qfs fname = toFile fileOptions fname $ do
+    layout_title .= title
+    traverse_ plotLine qfs
+  where
+    fileOptions = FileOptions (800, 600) SVG loadSansSerifFonts
+    plotLine qf = plot $ line (show qf)
+                              [toList $ fmap (qf2pd qf) quotes]
+    qf2pd qf q = (day q,
+                  realToFrac $ field2fun qf q :: Double)
