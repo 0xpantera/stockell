80b44819a728cd152fc730267673ee936a8e94cd
Francososa <franky.sosa@gmail.com>
2019-02-02 23:40:12 -0600

    Define Params Datatype and function to specify params and args correspondence

--- src/Params.hs
+++ src/Params.hs
@@ -6,3 +6,26 @@ import Data.Semigroup ((<>))
 import Options.Applicative
 
 
+data Params = Params { fname :: FilePath
+                     , company :: String
+                     , prices :: Bool
+                     , volumes :: Bool }
+
+
+mkParams :: Parser Params
+mkParams =
+  Params <$>
+             strArgument
+             (metavar "FILE" <> help "CSV file name")
+         <*> strOption
+             (long "company" <> short 'c'
+              <> help "stock company's name" <> value "")
+         <*> switch
+             (long "prices" <> short 'p'
+              <> help "create file with prices chart")
+         <*> switch
+             (long "volumes" <> short 'v'
+             <> help "create file with volumes chart")
+
+
+               
