b0e81c8da9c37c89044f42245663295a5b774519
Francososa <franky.sosa@gmail.com>
2019-02-02 23:53:17 -0600

    Create Main module with imports

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,16 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
+import Control.Monad (when)
+import qualified Data.Text.IO as TIO
+import qualified Data.ByteString.Lazy as BL (readFile)
+import Data.Csv (decodeByName)
+
+import QuoteData
+import Statistics
+import StatReport
+import Charts
+import Params
+  
 main :: IO ()
 main = print "Implement"
