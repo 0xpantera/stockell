8418baf4813228a40e1a276b756f507721328f41
Francososa <franky.sosa@gmail.com>
2019-02-02 19:19:38 -0600

    Define statistic types

--- src/Statistics.hs
+++ src/Statistics.hs
@@ -10,3 +10,19 @@ import Data.Time (diffDays)
 
 import BoundedEnum
 import QuoteData
+
+
+data Statistic =  Mean | Min | Max | Days
+  deriving (Show, Eq, Enum, Bounded, BoundedEnum)
+
+
+data StatEntry = StatEntry { stat :: Statistic,
+                             qfield :: QField,
+                             value :: Fixed4 }
+
+
+type StatQFieldData = (Qfield, [StatEntry])
+type StatInfo = [StatQFieldData]
+
+
+
